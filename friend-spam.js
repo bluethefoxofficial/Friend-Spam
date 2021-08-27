2456fcc63b5142176f4fbafc7a778a1a1f6554f9
bluethefoxofficial <47904806+bluethefoxofficial@users.noreply.github.com>
2021-08-27 02:08:37 +0100

    Simplified
    Added alert after the total

--- friend-spam.js
+++ friend-spam.js
@@ -1,18 +1,10 @@
  
-var Total = 0;
- if (document.all !== undefined)
-    {
-      var items = document.all;
-    }
-    else
-    {
-      var items = document.getElementsByTagName("*");
-    };                    
+  var Total = 0;
+      var items = document.getElementsByTagName("*");                    
         for (var i = 0; i < items.length; ++i) {
         if (items[i].textContent.includes("Add Friend")) {
             items[i].click();
-            total++;
+            Total++;
         }
-
-        alert("FS: Total of add friends clicked:" + total);
     }
+  alert("FS: Total of add friends clicked:" + Total);
